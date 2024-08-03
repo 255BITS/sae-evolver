37beb3c13a4afb1a3297791b83504e9b26ebfbba
Martyn Garcia <martyn.garcia@gmail.com>
2024-08-03 14:31:02 -0600

    parameterize layer

--- gemma/model_utils.py
+++ gemma/model_utils.py
@@ -54,24 +54,23 @@ def lazy_load_model_and_tokenizer():
     if tokenizer is None:
         tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b")
 
-def lazy_load_sae():
-    global sae
-    if sae is None:
-        path_to_params = hf_hub_download(
-            repo_id="google/gemma-scope-2b-pt-res",
-            filename="layer_20/width_16k/average_l0_71/params.npz",
-            force_download=False,
-        )
-        params = np.load(path_to_params)
-        pt_params = {k: torch.from_numpy(v).to("cuda") for k, v in params.items()}
-        sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
-        sae.load_state_dict(pt_params)
-        sae.cuda()
+def load_sae(target_layer):
+    path_to_params = hf_hub_download(
+        repo_id="google/gemma-scope-2b-pt-res",
+        filename=f"layer_{target_layer}/width_16k/average_l0_71/params.npz",
+        force_download=False,
+    )
+    params = np.load(path_to_params)
+    pt_params = {k: torch.from_numpy(v).to("cuda") for k, v in params.items()}
+    sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
+    sae.load_state_dict(pt_params)
+    sae.cuda()
+    return sae
 
 def process_prompt(prompt, target_layer=20):
     # Lazy load the model, tokenizer, and sae
     lazy_load_model_and_tokenizer()
-    lazy_load_sae()
+    sae = load_sae(target_layer)
 
     # Tokenize the input prompt
     inputs = tokenizer.encode(prompt, return_tensors="pt", add_special_tokens=True).cuda()
