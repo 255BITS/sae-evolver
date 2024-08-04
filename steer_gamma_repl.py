c5f6b66b04d54685fbcb4725c05f0538e7d55e01
Mikkel Garcia <mikkel@255bits.com>
2024-08-03 23:42:10 -0600

    steer_repl with examples

--- steer_gamma_repl.py
+++ steer_gamma_repl.py
@@ -5,25 +5,29 @@ from gemma.model_utils import steer_generate
 from sae_evolution import load_candidate
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python generate_prompt.py <yaml_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python generate_prompt.py <candidate_yamle> <example_yaml>")
         sys.exit(1)
 
-    yaml_file = sys.argv[1]
+    example_file = sys.argv[2]
+    candidate_yaml = sys.argv[1]
+    prompt_format = yaml.safe_load(example_yaml)["prompt_format"]
 
-    data = load_candidate(yaml_file)
+    data = load_candidate(candidate_yaml)
     while(True):
         user_input = input("Enter your input: ")
 
-        prompt = "User: "+user_input+"\nGPT:" 
-        #output = steer_generate(prompt, {})
-        #print("Generated output without steering:")
-        #print('--', data.layers)
-        #print(output)
+        prompt = prompt_format.replace("USER_INPUT", user_input)
+        output = steer_generate(prompt, {})
+        print("Generated output without steering:")
+        print("---")
+        print(output)
+        print("---")
 
         output = steer_generate(prompt, data.layers)
         
         print("Generated output with steering:")
+        print("---")
         print(output)
 
 if __name__ == "__main__":
